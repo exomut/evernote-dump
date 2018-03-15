c053d6d9db5b996c88bccf059b9b49fbe7773e1e
Seth Van Roekel <exomut@gmail.com>
2018-03-15 14:27:37 +0900

    Improved layout in kivy file.

--- source/main.py
+++ source/main.py
@@ -1,13 +1,37 @@
+import os
 import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-
+from kivy.config import Config
+##################
+# Configure Kivy #
+##################
 kivy.require('1.10.0')
+Config.set('graphics', 'width', '1200')
+Config.set('graphics', 'height', '700')
+Config.write()
+##################
+##################
 
 
 class Main(BoxLayout):
     def __init__(self):
         super(Main, self).__init__()
+        self.log_box = self.ids['log_box']
+        self.__file_list = []
+        self.__export_dir = ''
+        self.log_box.text = """Please choose .enex file(s) on the left and the output directory on the right."""
+
+    def update_export_dir(self, paths):
+        self.__export_dir = ''
+        if os.path.isdir(paths[0]):
+            self.__export_dir = paths[0]
+
+    def update_file_list(self, paths):
+        self.__file_list = []
+        for path in paths:
+            if '.enex' in path:
+                self.__file_list.append(path)
 
 
 class MainApp(App):
